5320dddc55d69be4664697e309b6e122d4268db2
Gan Xu <gan.xu@wustl.edu>
2020-11-22 23:14:11 -0600

    UPDATE

--- Citrix/how_many_sentences.py
+++
@@ -1,9 +0,0 @@
-"""
-https://www.1point3acres.com/bbs/thread-670144-1-1.html
-"""
-
-from collections import Counter
-
-wordSet = ["listen", "silent", "it", "is"]
-sentence = "listen it is silent"
-
