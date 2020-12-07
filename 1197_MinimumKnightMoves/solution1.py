9697c6267a3501670ccd32e47ff329c58a8a9b13
Gan Xu <gan.xu@wustl.edu>
2020-12-07 06:23:16 -0600

    Create solution1.py

---
+++ 1197_MinimumKnightMoves/solution1.py
@@ -0,0 +1,27 @@
+# bfs, tle
+
+from collections import deque
+
+
+def minKnightMoves(x: int, y: int) -> int:
+
+    # eight possible actions to go
+    actions = [[1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2]]
+
+    wl = deque([[0, 0, 0]])
+
+    while wl:
+
+        curr_x, curr_y, step = wl.popleft()
+        for action in actions:
+            new_pt = [ curr_x+action[0], curr_y+action[1], step+1]
+            if new_pt[:2] == [x, y]:
+                return new_pt[2]
+            else:
+                wl.append(new_pt)
+
+
+if __name__ == '__main__':
+    print(minKnightMoves(2,1))
+
+
