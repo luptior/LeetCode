fa034d44293abe6b19a42554b3a308164a446cf8
Gan Xu <gan.xu@wustl.edu>
2020-12-11 06:51:19 -0600

    update add new solutions

---
+++ 493_ReversePairs/solution2.py
@@ -0,0 +1,20 @@
+# a littlee bit better
+
+
+class Solution:
+    def reversePairs(self, nums: [int]) -> int:
+
+
+
+        return 0
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    print(s.reversePairs([1,3,2,3,1]))
--- 5_longestPalindromicSubstring/solution1.py
+++ 5_longestPalindromicSubstring/solution1.py
@@ -3,16 +3,23 @@
 
 def longestPalindrome(s: str) -> str:
 
-    result = ""
-    max_ln = 0
+    if len(s) < 10:
+        result = ""
+        max_ln = 0
 
-    for i in range(len(s)):
-        for j in range(i+1, len(s)+1):
-            if j-i > max_ln and s[i:j] == "".join(reversed(s[i:j])):
-                result = s[i:j]
-                max_ln = len(s[i:j])
+        for i in range(len(s)):
+            for j in range(i+1, len(s)+1):
+                if j-i > max_ln and s[i:j] == "".join(reversed(s[i:j])):
+                    result = s[i:j]
+                    max_ln = len(s[i:j])
 
-    return result
+        return result
+
+    if len(s) % 2 == 1:
+
+
+def expendAroundCenter(s, L, R):
+    while L >= 0 and R < len(s)
 
 
 if __name__ == '__main__':
