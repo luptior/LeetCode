0522578f60e4cc887211eec92fa54ba02b6b3ac6
Gan Xu <gan.xu@wustl.edu>
2021-02-07 21:42:51 -0600

    Update solution1.py

--- 146_LRUCache/solution1.py
+++ 146_LRUCache/solution1.py
@@ -28,3 +28,28 @@ class LRUCache(OrderedDict):
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
+
+if __name__ == '__main__':
+
+    s = None
+
+    instr = ["LRUCache", "put",  "put",  "get", "put",  "get", "put",  "get", "get", "get"]
+    param = [[2],        [1, 1], [2, 2], [1],   [3, 3], [2],   [4, 4], [1],    [3],   [4]]
+
+
+    for i in range(len(instr)):
+
+        this_instr = instr[i]
+        this_param = param[i]
+
+        if this_instr == "LRUCache":
+            s = LRUCache(this_param[0])
+        elif this_instr == "put":
+            k, v = this_param
+            s.put(k,v)
+        elif this_instr == "get":
+            print(s.get(this_param[0]))
+        else:
+            print("Wrong")
+
+
