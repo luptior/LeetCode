7c955bb8b7afc9030563e055163efbef84fa9c14
Gan Xu <gan.xu@wustl.edu>
2021-03-11 23:29:34 -0600

    Update solution1.py
    added lru_cache()

--- 983_Minimum_Cost_For_Tickets/solution1.py
+++ 983_Minimum_Cost_For_Tickets/solution1.py
@@ -3,47 +3,45 @@ recursive, TLE
 
 """
 
+
 class Solution:
 
     def __init__(self):
         self.days = []
         self.costs = []
 
-        self.mem = {}
-
-    def mincostTickets(self, days: [int], costs: [int]) -> int:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
         self.days = days
         self.costs = costs
 
-        self.mem = {}
+        return self.step(0, 0)
 
-        result = self.step(days, 0)
+    @lru_cache(None)
+    def step(self, curr_date, cost_so_far):
 
-        print(self.mem)
-
-        return result
-
-    def step(self, days_left, cost_so_far):
-
-        if not days_left:
+        if curr_date >= len(self.days):
             return cost_so_far
 
-        c1 = self.step(days_left[1:], cost_so_far + self.costs[0])
+        c1 = self.step(curr_date + 1, cost_so_far + self.costs[0])
 
-        new_days_left = days_left[:]
+        new_days_left = self.days[curr_date:]
         end_date = new_days_left[0] + 6
+        new_curr_date = curr_date
 
         while new_days_left and new_days_left[0] <= end_date:
             new_days_left.pop(0)
+            new_curr_date += 1
 
-        c2 = self.step(new_days_left, cost_so_far + self.costs[1])
+        c2 = self.step(new_curr_date, cost_so_far + self.costs[1])
 
-        new_days_left = days_left[:]
+        new_days_left = self.days[curr_date:]
         end_date = new_days_left[0] + 29
+        new_curr_date = curr_date
 
         while new_days_left and new_days_left[0] <= end_date:
             new_days_left.pop(0)
+            new_curr_date += 1
 
-        c3 = self.step(new_days_left, cost_so_far + self.costs[2])
+        c3 = self.step(new_curr_date, cost_so_far + self.costs[2])
 
         return min(c1, c2, c3)
