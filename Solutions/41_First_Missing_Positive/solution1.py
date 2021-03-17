150f823648e1345e9fda7e2d5229fc7313022311
Gan Xu <gan.xu@wustl.edu>
2021-03-17 13:40:39 -0500

    Update solution1.py

--- Solutions/41_First_Missing_Positive/solution1.py
+++ Solutions/41_First_Missing_Positive/solution1.py
@@ -1,6 +1,6 @@
 """
-Runtime: 36 ms, faster than 66.86% of Python3 online submissions for First Missing Positive.
-Memory Usage: 14.2 MB, less than 50.47% of Python3 online submissions for First Missing Positive.
+Runtime: 32 ms, faster than 87.71% of Python3 online submissions for First Missing Positive.
+Memory Usage: 14.3 MB, less than 17.03% of Python3 online submissions for First Missing Positive.
 """
 
 from typing import List
@@ -14,9 +14,9 @@ class Solution:
         maxi = max(nums)  # O(N)
 
         if maxi > 1:
-            nSet = set(nums)
+            nSet = set([n for n in nums if n > 0])
 
-            for num in range(1, maxi):  # O(N)
+            for num in range(1, len(nSet) + 1):  # O(N)
                 if num not in nSet:  # O(1)
                     return num
         elif maxi <= 0:
