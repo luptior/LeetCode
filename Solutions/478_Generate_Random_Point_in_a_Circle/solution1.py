e3779a77df595ad9f841fdcfb97f09fd3f1a9665
Gan Xu <gan.xu@wustl.edu>
2021-03-29 00:53:55 -0500

    Create solution1.py

---
+++ Solutions/478_Generate_Random_Point_in_a_Circle/solution1.py
@@ -0,0 +1,19 @@
+import random
+
+
+class Solution:
+
+    def __init__(self, radius: float, x_center: float, y_center: float):
+        self.radius = radius / 1
+        self.x_center = x_center
+        self.y_center = y_center
+
+    def randPoint(self) -> List[float]:
+        x = random.random() * 2 - 1
+        y = random.random() * 2 - 1
+
+        while x ** 2 + y ** 2 > 1:
+            x = random.random() * 2 - 1
+            y = random.random() * 2 - 1
+
+        return [self.x_center + self.radius * x, self.y_center + self.radius * y]
