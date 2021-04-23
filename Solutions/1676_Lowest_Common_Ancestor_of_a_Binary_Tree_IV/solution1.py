8718b213297a7c4dc446b3749aeb7616092a8e81
Gan Xu <gan.xu@wustl.edu>
2021-04-23 10:23:19 -0500

    add new solution

---
+++ Solutions/1676_Lowest_Common_Ancestor_of_a_Binary_Tree_IV/solution1.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', nodes: 'List[TreeNode]') -> 'TreeNode':
+
+        nodes = set(nodes)
+
+        def lca(node):
+
+            if not node:
+                return None
+
+            if node in nodes:
+                return node
+
+            left, right = lca(node.left), lca(node.right)
+
+            if left and right:
+                return node
+            return left or right
+
+        return lca(root)
+
