e85517b12e5adc6532641792fce4b79d64cdd459
Aspartame Xu <luptior@gmail.com>
2022-08-14 13:48:47 -0700

    update

--- Educative/Ace_Python_Interview/1_Data_Structure/Lists/4_List_of_Products_of_all_Elements.py
+++ Educative/Ace_Python_Interview/1_Data_Structure/Lists/4_List_of_Products_of_all_Elements.py
@@ -15,24 +15,49 @@ arr = [24,12,8,6]
 """
 from math import prod
 
+"""
+O(n) solution
+"""
+
 
 def find_product(lst):
-
     left_prod = 1
     right_prod = prod(lst[1:])
 
     result = [right_prod]
 
     for i in range(1, len(lst)):
-        left_prod *= lst[i-1]
-        right_prod = right_prod//lst[i] if right_prod != 0 else 0
+        left_prod *= lst[i - 1]
+        right_prod = right_prod // lst[i] if right_prod != 0 else 0
         print(left_prod, right_prod)
-        result.append(right_prod*left_prod)
+        result.append(right_prod * left_prod)
 
     return result
+
+"""
+better o(n) without the division which is slower
+"""
+
+def find_product2(lst):
+    # get product start from left
+    left = 1
+    product = []
+    for ele in lst:
+        product.append(left)
+        left = left * ele
+
+    print(product)
+    # get product starting from right
+    right = 1
+    for i in reversed(range(len(lst))):
+        product[i] = product[i] * right
+        right = right * lst[i]
+        print(product)
+
+    return product
 
 
 if __name__ == '__main__':
     arr = [1, 2, 3, 4]
 
-    print(find_product(arr))
+    print(find_product2(arr))
